c27dd5910ef9fef7cfd4e160427a19c1be2a4d13
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2020-09-04 12:01:29 +0200

    created my first component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="mycomponent">
+    <hr />
+    <h1>Hello World Again!</h1>
+    <h2>{{subtitle}}</h2>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      subtitle: "this is only a subtitle",
+    };
+  },
+};
+</script>
+
